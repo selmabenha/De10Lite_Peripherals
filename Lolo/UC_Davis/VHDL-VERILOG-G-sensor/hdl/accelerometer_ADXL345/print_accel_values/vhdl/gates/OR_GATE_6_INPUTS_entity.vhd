095cbc6e70531685f4b8bc59cf67ffeb84a6369c
Lolo <loicfinette@gmail.com>
2024-03-14 22:32:41 +0100

    the program to read the values works fine. Starting to implement another program to determine the tilt of the DE10Lite

---
+++ Lolo/UC_Davis/VHDL-VERILOG-G-sensor/hdl/accelerometer_ADXL345/print_accel_values/vhdl/gates/OR_GATE_6_INPUTS_entity.vhd
@@ -0,0 +1,26 @@
+--==============================================================================
+--== Logisim-evolution goes FPGA automatic generated VHDL code                ==
+--== https://github.com/logisim-evolution/                                    ==
+--==                                                                          ==
+--==                                                                          ==
+--== Project   : accelerometer_ADXL345                                        ==
+--== Component : OR_GATE_6_INPUTS                                             ==
+--==                                                                          ==
+--==============================================================================
+
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+
+ENTITY OR_GATE_6_INPUTS IS
+   GENERIC ( BubblesMask : std_logic_vector );
+   PORT ( input1 : IN  std_logic;
+          input2 : IN  std_logic;
+          input3 : IN  std_logic;
+          input4 : IN  std_logic;
+          input5 : IN  std_logic;
+          input6 : IN  std_logic;
+          result : OUT std_logic );
+END ENTITY OR_GATE_6_INPUTS;
---
+++ Lolo/UC_Davis/VHDL-VERILOG-G-sensor/hdl/johnson/print_accel_values/vhdl/gates/AND_GATE_3_INPUTS_entity.vhd
@@ -0,0 +1,23 @@
+--==============================================================================
+--== Logisim-evolution goes FPGA automatic generated VHDL code                ==
+--== https://github.com/logisim-evolution/                                    ==
+--==                                                                          ==
+--==                                                                          ==
+--== Project   : johnson                                                      ==
+--== Component : AND_GATE_3_INPUTS                                            ==
+--==                                                                          ==
+--==============================================================================
+
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+
+ENTITY AND_GATE_3_INPUTS IS
+   GENERIC ( BubblesMask : std_logic_vector );
+   PORT ( input1 : IN  std_logic;
+          input2 : IN  std_logic;
+          input3 : IN  std_logic;
+          result : OUT std_logic );
+END ENTITY AND_GATE_3_INPUTS;
